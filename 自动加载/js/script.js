1f9325cb6d4e7f722cfc0f67ecfceb52139c623d
zhuangzhuangwpz <1074207241@qq.com>
2017-12-07 10:22:36 +0800

    微场景

---
+++ 自动加载/js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+});
