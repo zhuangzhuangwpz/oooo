3dcf09ceab6e77130641f3620ef55091f9c1a951
zhuangzhuangwpz <1074207241@qq.com>
2017-12-11 14:20:46 +0800

    动画

--- 自动加载/js/script.js
+++ 自动加载/js/script.js
@@ -7,4 +7,13 @@ var swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
     },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
