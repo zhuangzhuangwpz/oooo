a041b53d105fdf26e576b3579a3a293422d8abb3
zhuangzhuangwpz <1074207241@qq.com>
2017-12-14 17:19:44 +0800

    双选会

---
+++ 微场景/盛邦双选会邀请函/js/script.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper('.swiper-container',{
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+});
+//音乐
+var mp3 = document.querySelector("#mp3");
+var mp3btn = document.querySelector("#mp3btn");
+
+mp3btn.addEventListener('click', function () {
+    if (mp3.paused) {
+        mp3.play()
+        mp3btn.style.animationPlayState = 'running';
+    } else {
+        mp3.pause()
+        mp3btn.style.animationPlayState = 'paused';
+    }
+})
